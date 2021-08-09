fa23892532f5fe14de851fff196ebbbbd5f284ba
sam-ani12 <88648550+sam-ani12@users.noreply.github.com>
2021-08-09 20:26:33 +0530

    Add files via upload

---
+++ com.java
@@ -0,0 +1,59 @@
+package exception;
+import java.util.Scanner;
+
+ 
+
+public class Test {
+    public static void main(String[] args)  {
+
+ 
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int a =sc.nextInt();
+        if(a>100) {
+            System.out.println("fine");
+        }else {
+            try {
+                throw new MyException("enter value greater than 100");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        
+        
+    }
+
+ 
+
+}
+
+ 
+
+//custom exception  - user defined exception 
+//that is not previously avaivalble
+class MyException extends Exception {
+    public MyException() {
+
+ 
+
+    }
+
+ 
+
+    MyException(String message) {
+        super(message);
+    }
+}
+
+ 
+
+ 
+
+ 
+
+
+ 
+public class com {
+
+}
